e661678922c57bb9342b5e95d37eec44a1abb325
Master <Master@DESKTOP-S0GM9LR>
2020-11-20 17:27:16 +0900

    Step04_Class 추가됨

--- Step04_Class/src/test/mypac/MyUtil.java
+++ Step04_Class/src/test/mypac/MyUtil.java
@@ -3,6 +3,12 @@ package test.mypac;
  * 	static 자원을 포함하는 용도로 사용하기
  */
 public class MyUtil {
+	// non static 필드 (허상)
+	public int version=1;
+	
+	// static 에서는 this 가 의미가 없음 (this 가 아닌 클래스명을 언급하면됨.)
+	
+	// static 필드 (진짜)
 	public static String owner;
 	public static void playMusic() {
 		System.out.println("음악을 틀어요!");
