42227e7aa989eb6c5a9bfb1f61a29bbd511821e5
Master <Master@DESKTOP-S0GM9LR>
2020-12-02 18:06:42 +0900

    Step16_Thread

--- Step02_DataType/src/test/main/MainClass05.java
+++ Step02_DataType/src/test/main/MainClass05.java
@@ -5,8 +5,8 @@ package test.main;
  *  2. 논리형 (boolean)
  *  
  *  - true, false 두가지 값중 하나이다.
- *  - true, false  를 직접 쓰거나 비교 연산 혹은 논리 연산의 결과로 얻어 낼수 있다.
- *  - 참고 거짓을 나타내는 데이터 type  
+ *  - true, false 를 직접 쓰거나 비교 연산 혹은 논리 연산의 결과로 얻어 낼수 있다.
+ *  - 참과 거짓을 나타내는 데이터 type  
  */
 public class MainClass05 {
 	public static void main(String[] args) {
