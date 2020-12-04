96252027f3a3e9872e650d26cb0b01e2147ee10f
Master <Master@DESKTOP-S0GM9LR>
2020-12-04 12:26:41 +0900

    Step17_InputOutput 추가됨

--- Step17_InputOutput/src/test/main/MainClass16.java
+++ Step17_InputOutput/src/test/main/MainClass16.java
@@ -5,8 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.Buffer;
 
-public class MainClass15 {
+public class MainClass16 {
 	public static void main(String[] args) {
 		/*	FileInputStream 객체를 이용해서
 		 *  c:/myFolder/1.jpg 에서 byte 데이터를 읽어서 
@@ -24,16 +25,21 @@ public class MainClass15 {
 			fis=new FileInputStream(new File("c:/myFolder/1.jpg"));
 			// 파일로부터 byte 알갱이를 읽어들이는 객체 생성
 			fos=new FileOutputStream(new File("c:/myFolder/copied.jpg"));
+			
+			// 한번에 byte 알갱이 1024개를 저장할 수 있는 배열 객체 생성
+			byte[] buffer=new byte[1024];
 			while(true) {
-				int data=fis.read();  // 1byte 읽어들이기
-				if(data==-1) {  // 더이상 읽을 데이터가 없으면
-					break;  // 탈출
+				// byte 배열의 참조값을 read() 메소드에 전달해서 byte 데이터를
+				// 배열에 담고, 읽은 갯수랄 참조받는다.
+				int readedCount=fis.read(buffer);
+				if(readedCount==-1) {
+					break;
 				}
-				
-				fos.write(data);  // 출력할 준비
-				fos.flush();  // 방출 (실제 출력되는 시점)
+				// FileOutputStream 객체를 이용해서
+				// byte 배열에 담긴 데이터를 0번 인덱스로부터 읽은 갯수만큼 출력한다.
+				fos.write(buffer, 0, readedCount);
 			}
-			System.out.println("파일을 성공적으로 복사 했습니다.");
+			System.out.println("파일을 성공적으로 복사했습니다.");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
